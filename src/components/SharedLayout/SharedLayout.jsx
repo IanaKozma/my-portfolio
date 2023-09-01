6511c16adbee13e2f372c25d0772fbbdd6b650b4
Iana Kozma <yana.kozma@gmail.com>
2023-09-01 16:32:16 +0200

    added header

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,15 +1,13 @@
 import { Outlet } from "react-router-dom";
-import { Container, Header, Logo, Link } from "./SharedLayout.styled";
+import logo from '../../images/code-girl.png';
+import sprite from "../../images/sprite.svg";
+import { Container, Header, Link, SocialIcons, SocialIcon } from "./SharedLayout.styled";
 
 export const SharedLayout = () => {
   return (
     <Container>
-      <Header>
-        <Logo>
-          <span role="img" aria-label="computer icon">
-            💻
-          </span>
-        </Logo>
+      <Header>     
+        <img src={logo} alt="Logo" width="97" height="59" />
         <nav>
             <Link to="/">Home</Link>
             <Link to="/about">About</Link>
@@ -17,6 +15,30 @@ export const SharedLayout = () => {
             <Link to="/projects">Projects</Link>
             <Link to="/contact">Contact</Link>
         </nav>
+        <SocialIcons>
+          <li>
+            <SocialIcon
+              href="https://github.com/IanaKozma"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <svg width="30" height="30">
+                <use href={sprite + "#icon-github"}></use>
+              </svg>
+            </SocialIcon>
+          </li>
+          <li>
+            <SocialIcon
+              href="https://www.linkedin.com/in/iana-kozma/"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <svg width="30" height="30">
+                <use href={sprite + "#icon-linkedin"}></use>
+              </svg>
+            </SocialIcon>
+          </li>
+        </SocialIcons>
       </Header>
       <Outlet />
     </Container>
--- src/components/SharedLayout/SharedLayout.styled.jsx
+++ src/components/SharedLayout/SharedLayout.styled.jsx
@@ -2,9 +2,23 @@ import styled from "styled-components";
 import { NavLink } from "react-router-dom";
 
 export const Container = styled.div`
-  max-width: 1600px;
-  margin: 0 auto;
-  padding: 0 195px;
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0 60px;
+  background-color: var(--dark-mode-color);
+
+  @media screen and (min-width: 480px) {
+    width: 480px;
+  }
+
+  @media screen and (min-width: 768px) {
+    width: 768px;
+  }
+
+  @media screen and (min-width: 1200px) {
+    width: 1200px;
+  }
 `;
 
 export const Header = styled.header`
@@ -14,27 +28,39 @@ export const Header = styled.header`
   gap: 12px;
   padding: 8px 0;
   margin-bottom: 16px;
-  border-bottom: 1px solid black;
 
   > nav {
     display: flex;
   }
 `;
 
-export const Logo = styled.p`
-  font-weight: 700;
-  margin: 0;
-`;
-
 export const Link = styled(NavLink)`
   padding: 8px 16px;
-  border-radius: 4px;
   text-decoration: none;
-  color: black;
+  color: var(--nav-subtitle-color);
+  font-family: var(--secondary-medium);
   font-weight: 500;
+  font-size: 20px;
+  line-height: 26px;
 
+  &:hover,
+  &:focus,
   &.active {
-    color: white;
-    background-color: orangered;
+    color: var(--nav-hover-color);
+  }
+`;
+
+export const SocialIcons = styled.ul`
+  display: flex;
+  gap: 22px;
+  padding: 8px 0;
+`;
+
+export const SocialIcon = styled.a`
+  fill: var(--nav-subtitle-color);
+
+  &:hover {
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    fill: var(--white-color);
   }
 `;
