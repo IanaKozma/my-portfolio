87df429a30bc114c60b8351fca6ad1d7a63c6d98
Iana Kozma <yana.kozma@gmail.com>
2023-10-09 13:02:00 +0200

    added projects cards

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,23 +1,25 @@
 import webstudio from "../../images/webstudio.jpg";
+import mimino from "../../images/mimino.jpg";
+import news from "../../images/news.jpg";
+import phonebook from "../../images/phonebook.jpg";
 import sprite from "../../images/sprite.svg";
 import { Container } from "../Header/Header.styled";
 import { ProjectsPageTitle } from "../TechStack/TechStack.styled";
-import { ProjectItem, ProjectImg, ProjectWrapper, ProjectTitle, ProjectDescription, ProjectStack, ProjectLinksList, ProjectLink, ProjectIcon } from "./Projects.styled";
+import { Section, ProjectsList, ProjectItem, ProjectImg, ProjectWrapper, ProjectTitle, ProjectDescription, ProjectType, ProjectStack, StackTitle, ProjectLinksList, ProjectLink, ProjectIcon } from "./Projects.styled";
 
 export const MyProjects = () => {
     return (
         <Container>
-            <section>
+            <Section>
                 <ProjectsPageTitle>Projects</ProjectsPageTitle>
-                <ul>
+                <ProjectsList>
                     <ProjectItem>
-                        <ProjectImg src={webstudio} alt="Webstudio" width="375" />
+                        <ProjectImg src={webstudio} alt="Webstudio" width="325" />
                         <ProjectWrapper>
                             <ProjectTitle>WebStudio</ProjectTitle>
                             <ProjectDescription>A website of WebStudio company. Responsive design, transform effects, modal windows.</ProjectDescription>
-                            <ProjectDescription>Role: developer</ProjectDescription>
-                            <ProjectDescription>Individual project.</ProjectDescription>
-                            <ProjectStack>Tech stack: HTML, SASS, JS</ProjectStack>
+                            <ProjectType>Individual project.</ProjectType>
+                            <ProjectStack><StackTitle>Tech stack</StackTitle>: HTML, SASS, JS</ProjectStack>
                             <ProjectLinksList>
                                 <li>
                                     <ProjectLink
@@ -46,8 +48,113 @@ export const MyProjects = () => {
                             </ProjectLinksList>
                         </ProjectWrapper>
                     </ProjectItem>
-                </ul>
-            </section>
+                    <ProjectItem>
+                        <ProjectImg src={mimino} alt="Mimino" width="325" />
+                        <ProjectWrapper>
+                            <ProjectTitle>Mimino</ProjectTitle>
+                            <ProjectDescription>A website of Mimino Hotel, Restaurant. Adaptive design, sliders, modal windows.</ProjectDescription>
+                            <ProjectType>Team project (Restaurant Menu section).</ProjectType>
+                            <ProjectStack><StackTitle>Tech stack</StackTitle>: HTML, SASS, JS, Parcel</ProjectStack>
+                            <ProjectLinksList>
+                                <li>
+                                    <ProjectLink
+                                        href="https://alexkabaiev.github.io/project-13/"
+                                        target="_blank"
+                                        rel="noreferrer noopener"
+                                    >
+                                        <ProjectIcon width="20" height="20">
+                                            <use href={sprite + "#icon-link-chain"}></use>
+                                        </ProjectIcon>
+                                        Live Preview
+                                    </ProjectLink>
+                                </li>
+                                <li>
+                                    <ProjectLink
+                                        href="https://github.com/AlexKabaiev/project-13"
+                                        target="_blank"
+                                        rel="noreferrer noopener"
+                                    >
+                                        <ProjectIcon width="20" height="20">
+                                            <use href={sprite + "#icon-github"}></use>
+                                        </ProjectIcon>
+                                        View Code
+                                    </ProjectLink>
+                                </li>
+                            </ProjectLinksList>
+                        </ProjectWrapper>
+                    </ProjectItem>
+                    <ProjectItem>
+                        <ProjectImg src={news} alt="News" width="325" />
+                        <ProjectWrapper>
+                            <ProjectTitle>News App</ProjectTitle>
+                            <ProjectDescription>Adaptive design, news/weather API requests, pagination,theme change, calendar, burger menu.</ProjectDescription>
+                            <ProjectType>Team project (Weather Forecast section).</ProjectType>
+                            <ProjectStack><StackTitle>Tech stack</StackTitle>: HTML, SASS, JS, Parcel</ProjectStack>
+                            <ProjectLinksList>
+                                <li>
+                                    <ProjectLink
+                                        href="https://tkachenko01001.github.io/news-project/"
+                                        target="_blank"
+                                        rel="noreferrer noopener"
+                                    >
+                                        <ProjectIcon width="20" height="20">
+                                            <use href={sprite + "#icon-link-chain"}></use>
+                                        </ProjectIcon>
+                                        Live Preview
+                                    </ProjectLink>
+                                </li>
+                                <li>
+                                    <ProjectLink
+                                        href="https://github.com/Tkachenko01001/news-project"
+                                        target="_blank"
+                                        rel="noreferrer noopener"
+                                    >
+                                        <ProjectIcon width="20" height="20">
+                                            <use href={sprite + "#icon-github"}></use>
+                                        </ProjectIcon>
+                                        View Code
+                                    </ProjectLink>
+                                </li>
+                            </ProjectLinksList>
+                        </ProjectWrapper>
+                    </ProjectItem>
+                    <ProjectItem>
+                        <ProjectImg src={phonebook} alt="Phonebook" width="325" />
+                        <ProjectWrapper>
+                            <ProjectTitle>Phonebook App</ProjectTitle>
+                            <ProjectDescription>Phonebook app on React/Redux Toolkit, user registration, login, add/delete contacts.</ProjectDescription>
+                            <ProjectType>Individual project.</ProjectType>
+                            <ProjectStack><StackTitle>Tech stack</StackTitle>: HTML, Emotion, React/Redux</ProjectStack>
+                            <ProjectLinksList>
+                                <li>
+                                    <ProjectLink
+                                        href="https://ianakozma.github.io/goit-react-hw-08-phonebook/"
+                                        target="_blank"
+                                        rel="noreferrer noopener"
+                                    >
+                                        <ProjectIcon width="20" height="20">
+                                            <use href={sprite + "#icon-link-chain"}></use>
+                                        </ProjectIcon>
+                                        Live Preview
+                                    </ProjectLink>
+                                </li>
+                                <li>
+                                    <ProjectLink
+                                        href="https://github.com/IanaKozma/goit-react-hw-08-phonebook"
+                                        target="_blank"
+                                        rel="noreferrer noopener"
+                                    >
+                                        <ProjectIcon width="20" height="20">
+                                            <use href={sprite + "#icon-github"}></use>
+                                        </ProjectIcon>
+                                        View Code
+                                    </ProjectLink>
+                                </li>
+                            </ProjectLinksList>
+                        </ProjectWrapper>
+                    </ProjectItem>
+                </ProjectsList>
+            </Section>
         </Container>
     );
 };
--- src/components/Projects/Projects.styled.jsx
+++ src/components/Projects/Projects.styled.jsx
@@ -1,7 +1,39 @@
 import styled from "styled-components";
 
+export const Section = styled.section`
+    padding-bottom: 60px;
+`;
+
+export const ProjectsList = styled.ul`
+    @media screen and (min-width: 768px) {
+        display: flex;
+        flex-wrap: wrap;
+        gap: 30px;
+    }
+`;
+
 export const ProjectItem = styled.li`
-    /* border-radius: 20px; */
+    width: 100%;
+    transition: transform 0.25s;
+
+    &:hover {
+        cursor: pointer;
+        transform: scale(1.05)
+    }
+
+    @media screen and (max-width: 767px) {
+        &:not(:last-child) {
+            margin-bottom: 30px;
+        }
+    }
+    @media screen and (min-width: 768px) {
+        flex-basis: calc((100% - 30px) / 2);
+        width: 354px;
+    }
+    @media screen and (min-width: 1200px) {
+        flex-basis: calc((100% - 60px) / 3);
+        width: 370px;
+    }
 `;
 
 export const ProjectImg = styled.img`
@@ -10,25 +42,31 @@ export const ProjectImg = styled.img`
 `;
 
 export const ProjectWrapper = styled.div`
-    width: 325px;
-    padding: 25px;
+    width: 295px;
+    padding: 15px;
     background-color: var(--project-bg-color);
     border-bottom-left-radius: 20px;
     border-bottom-right-radius: 20px;
 `;
 
 export const ProjectTitle = styled.h3`
-    margin-bottom: 17px;
+    margin-bottom: 15px;
     font-family: var(--primary-medium);
-    font-size: 28px;
-    line-height: 26px;
+    font-size: 26px;
+    line-height: 24px;
     color: var(--title-color);
 `;
 
 export const ProjectDescription = styled.p`
     font-family: var(--primary-light);
-    font-size: 18px;
-    line-height: 26px;
+    font-size: 16px;
+    line-height: 24px;
+    color: var(--title-color);
+`;
+
+export const ProjectType = styled(ProjectDescription)`
+    height: 35px;
+    margin-top: 12px;
     color: var(--title-color);
 `;
 
@@ -41,6 +79,10 @@ export const ProjectStack = styled.p`
     color: var(--title-color);
 `;
 
+export const StackTitle = styled.span`
+    text-decoration: underline;
+`;
+
 export const ProjectLinksList = styled.ul`
     display: flex;
     align-items: center;
@@ -51,15 +93,15 @@ export const ProjectLink = styled.a`
     display: flex;
     align-items: center;
     font-family: var(--primary-regular);
-    font-size: 16px;
-    line-height: 26px;
+    font-size: 14px;
+    line-height: 24px;
     color: var(--white-color);
     fill: var(--white-color);
 
     &:hover,
     &:focus {
-        color: var(--nav-hover-color);
-        fill: var(--nav-hover-color);
+        color: var(--project-link-color);
+        fill: var(--project-link-color);
     }
 `;
 
