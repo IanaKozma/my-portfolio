bff91855fb358e133bbe4aab649f53547430ac37
Iana Kozma <yana.kozma@gmail.com>
2023-10-22 16:13:43 +0200

    updated server.js

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '../build')));
 app.use(cors());
 app.use(bodyParser.json());
 
@@ -33,7 +33,7 @@ app.post('/my-portfolio/contact', (req, res) => {
 });
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3001;
